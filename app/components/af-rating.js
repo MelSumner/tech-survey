1f919e78d41e35b4a294fce17fd9e8f77c781ed3
Melanie Sumner <melaniersumner@gmail.com>
2019-04-26 09:51:46 -0500

    add rating component

--- app/components/af-rating.js
+++ app/components/af-rating.js
@@ -1,9 +1,26 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
     attributeBindings: ['tabindex'],
     tabindex: -1,
     didRender() {
         this.element.focus();
+    },
+
+    options: computed('model.{min,max}', function() {
+        let options = [];
+
+        for (let i = this.model.min; i <= this.model.max; i++) {
+            options.push(i);
+        }
+
+        return options;
+    }),
+
+    actions: {
+        changed(value) {
+            this.set('answerValue', value);
+        }
     }
 });
