5275aff09dfec314d4136eee860e0bffccd10c82
Melanie Sumner <melaniersumner@gmail.com>
2019-04-26 09:40:24 -0500

    route to index if attempted route doesn't exist

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,6 +2,12 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params) {
-        return this.store.peekRecord('question', params.id);
+        let question = this.store.peekRecord('question', params.id);
+
+        if(!question) {
+            this.transitionTo('index');
+        }
+
+        return question;
     }
 });
